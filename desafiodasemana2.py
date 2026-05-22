a04f42b75aed61db81d6efe1ae140efd1c9612e0
João Marcelo <joaomarcelob.comercial@gmail.com>
2026-05-22 16:27:49 -0300

    Forçando que a entrada de dados tipo INT sejam maiores que 0

--- desafiodasemana2.py
+++ desafiodasemana2.py
@@ -1,15 +1,27 @@
 #entrada de dados
-#monstro1
-nome1="Mago Negro"
-hp1=1700
-atk1=450
+hp1=999
+hp2=999
+while (hp1 and hp2) > 0:
+    valida = 1
+    while valida == 1:
+        #monstro1
+        nome1=input("Nome do Monstro:")
+        hp1=int(input("Valor do hp:"))
+        atk1=int(input("Valor do atk:"))
 
-#monstro2
-nome2="Dragão Negro de Olhos Vermelhos"
-hp2=2400
-atk2=350
+        #monstro2
+        nome2=input("Nome do Monstro:")
+        hp2=int(input("Valor do hp:"))
+        atk2=int(input("Valor do atk:"))
+        
+        #pede pro usuário escrever numero acima de 0 pra atk e hp
+        if hp1 <= 0 or hp2 <= 0 or atk1 <= 0 or atk2 <= 0:
+            print("ESCREVE DIREITO, VALOR POSITIVO MANO")
+        
+        
+            
 
 
-print(nome1,"\nataque:",atk1,"\nhp:",hp1,"\n")
-print(nome2,"\nataque:",atk2,"\nhp:",hp2)
+    #print(nome1,"\nataque:",atk1,"\nhp:",hp1,"\n")
+    #print(nome2,"\nataque:",atk2,"\nhp:",hp2)
 
