a9a3cc5b582a6fbbe6df835e677b4a14e4a61d02
João Marcelo <joaomarcelob.comercial@gmail.com>
2026-05-22 17:25:49 -0300

    Finalizar em casa

--- desafiodasemana2.py
+++ desafiodasemana2.py
@@ -19,42 +19,68 @@ while valida == 1:
     if hp1 <= 0 or hp2 <= 0 or atk1 <= 0 or atk2 <= 0:
         print("ESCREVE DIREITO, VALOR POSITIVO MANO")
     else:
-        valida = 0    
-            
-print("teste")    
-        
-            
-
-
-    #print(nome1,"\nataque:",atk1,"\nhp:",hp1,"\n")
-    #print(nome2,"\nataque:",atk2,"\nhp:",hp2)
+        #LUTA =========== BATALHA ============= DUELO
+        while hp1>0 and hp2>0:
+            print("Que comece o duelo!")
+            print(ataque())
+            if ataque() == 0:
+                print("Final do duelo")
+            else:
+                print(contraataque())
+                if contraataque() == 0:
+                    print("Final do duelo")
 
 
 #FUNÇÕES
-def ataque(ataque,vida):
-    hp = vida-ataque
-    print(f"O",nome1,"atacou",nome2,"deixando ele com",hp,"de vida!")
-    return(hp)
-    
-    
-def contraataque(contraataque,vida):
-    hp = vida-contraataque
-    print(f"O",nome2,"contra atacou",nome1,"deixando ele com",hp,"de vida!")
-    return(hp)
 
-def batalha(atk1,atak2,hp1,hp2):
+def ataque():
+    hp = hp2 - atk1
+    
+    if hp <= 0:
+        print(f"O", nome2,"desmaiou!\n",nome1,"é o vencedor!")       
+        return(0)
+    else:
+        print(f"O",nome1,"atacou",nome2,"deixando ele com",hp,"de vida!")
+        print("A batalha continua!") 
+        return(hp) 
+        
+    
+    
+def contraataque():
+    hp = hp1 - atk2
+    
+    if hp <= 0:
+        print(f"O", nome1,"desmaiou!\n",nome2,"é o vencedor!")       
+        return(0)
+    else:
+        print(f"O",nome2,"atacou",nome1,"deixando ele com",hp,"de vida!")
+        print("A batalha continua!") 
+        return(hp) 
+    
+    
+    
+    
+    
+    
+    
+    
+'''
+def batalha():
     novohp2 = hp2 - atk1
-    novohp1 = hp1 - atak2
+    novohp1 = hp1 - atk2
     
     
     #verificação de vitória
     if novohp1 <= 0 and novohp2 <=0:
         print("Ambos desmaiaram! Tivemos um empate!")
+        novohp1 == novohp2 == 0
+        return()
     elif novohp1 <= 0:
         print(f"O", nome1,"desmaiou!\n",nome2,"é o vencedor!")
+        
     elif novohp2 <= 0:
         print(f"O", nome2,"desmaiou!\n",nome1,"é o vencedor!")
     else:
         print(f"O",nome1,"está com",novohp1,"de hp\n","O",nome2,"está com",novohp2,"de hp")
-        print("A batalha continua!")
+        print("A batalha continua!")'''
     
