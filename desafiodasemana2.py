b9cb8e7cbcf8341252d377fbd9158ce3a6192b53
João Marcelo <joaomarcelob.comercial@gmail.com>
2026-05-22 15:50:33 -0300

    Criação das entradas de dados

---
+++ desafiodasemana2.py
@@ -0,0 +1,15 @@
+#entrada de dados
+#monstro1
+nome1="Mago Negro"
+hp1=1700
+atk1=450
+
+#monstro2
+nome2="Dragão Negro de Olhos Vermelhos"
+hp2=2400
+atk2=350
+
+
+print(nome1,"\nataque:",atk1,"\nhp:",hp1,"\n")
+print(nome2,"\nataque:",atk2,"\nhp:",hp2)
+
